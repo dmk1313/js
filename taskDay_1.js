2b99c8d93761b0698aebbfb4d8aff32ebb42b1ae
dmk1313 <chestar7@gmail.com>
2022-04-20 16:17:45 +0300

    cor 4

--- taskDay_1.js
+++ taskDay_1.js
@@ -1,16 +1,15 @@
-let stroka;
 let a;
 const hesh = function(stroka){
-
-    // if (stroka.trim() == ''){
-    //     return false;
-    // }
+    
+    if (!stroka.trim()){
+        return false;
+    }
     a = stroka.split(/\s/).map(word => word[0].toUpperCase() + word.substring(1)).join("");
-    if (a.length > 140){
+    if (a.length > 139){
         return false;
     }
     else {
         console.log("#" + a);
     }
 }
-hesh("sadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdfsadfasdfa asd fa sdf")
+hesh("asdf asdf sdf")
